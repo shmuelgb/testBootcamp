7f66a3172bc560f815b5bdc815e4ffc817c3acc2
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2022-02-01 11:07:30 +0200

    Update index.js

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -4,25 +4,25 @@ const fs = require("fs/promises");
 const scrape = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://sad-wescoff-918f8d.netlify.app/");
+  await page.goto("https://www.02ws.co.il/");
 
-  const selector = "#root a";
-  const text = await page.$$eval(selector, (x) => {
-    console.log(x[0].attributes[1].value);
-    return x.map((a) => a.attributes[1].value);
+  const selector = ".forcast_text";
+  const text = await page.$eval(selector, (x) => {
+    console.log(x.textContent);
+    return x.textContent;
   });
 
-  const photos = await page.$$eval("img", (imgs) => {
-    return imgs.map((img) => img.src);
-  });
+  // const photos = await page.$$eval("img", (imgs) => {
+  //   return imgs.map((img) => img.src);
+  // });
 
-  for (const photo of photos) {
-    const imagePage = await page.goto(photo);
-    await fs.writeFile(photo.split("/").pop(), await imagePage.buffer());
-  }
+  // for (const photo of photos) {
+  //   const imagePage = await page.goto(photo);
+  //   await fs.writeFile(photo.split("/").pop(), await imagePage.buffer());
+  // }
 
   await fs.writeFile("test.txt", text.toString());
 
-  await browser.close();
+  // await browser.close();
 };
 scrape();
