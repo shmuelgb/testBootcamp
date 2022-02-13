aa1b6fdf45b04f7b46eebfea2da439b2563c83d5
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2022-02-13 15:51:44 +0200

    Update interview.js

--- interview.js
+++ interview.js
@@ -234,3 +234,10 @@ function miniMaxSum(arr) {
 //   return arr[[arr.length - 1]].length;
 
 // console.log(lengthOfLastWord("hello world"));
+//!
+var plusOne = function (digits) {
+  digits = parseInt(digits.join(""));
+  digits++;
+  return Array.from(String(digits), Number);
+};
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
